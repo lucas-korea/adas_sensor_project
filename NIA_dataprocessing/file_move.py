d8743043fe18dba5abf074c6966a83dc96c33f73
lucas-korea <jcy37012@naver.com>
2021-08-30 13:53:29 +0900

    processing_and_matcing ver2 maded

--- NIA_dataprocessing/file_move.py
+++ NIA_dataprocessing/file_move.py
@@ -1,13 +1,41 @@
 import os
+import numpy as np
+from tkinter import filedialog
+from tkinter import messagebox
 import shutil
 
-path_dir = "E:\\20210826_005023\\20210826_005023\\PCDPNGfiles_matched"
-move_dir = "E:\\20210826_005023\\20210826_005023\\PCDPNGfiles"
-file_list = [file for file in os.listdir(path_dir) if file.endswith(".png")]
-print(len(file_list))
-png_i = 0
-for file_name in file_list:
-    if png_i % 10 == 5 or png_i % 10 == 0 or 1:
-        print(png_i)
-        shutil.move(path_dir + '\\' + file_name, move_dir + '\\' + file_name)
-    png_i = png_i + 1
+# path_dir = "E:\\20210826_005023\\20210826_005023\\PCDPNGfiles_matched"
+# move_dir = "E:\\20210826_005023\\20210826_005023\\PCDPNGfiles"
+# file_list = [file for file in os.listdir(path_dir) if file.endswith(".png")]
+# print(len(file_list))
+# png_i = 0
+# for file_name in file_list:
+#     if png_i % 10 == 5 or png_i % 10 == 0 or 1:
+#         print(png_i)
+#         shutil.move(path_dir + '\\' + file_name, move_dir + '\\' + file_name)
+#     png_i = png_i + 1
+
+
+def select_folder(str_):
+    folder = filedialog.askdirectory(initialdir=os.getcwd(),
+                                        title=str_)
+    if folder == '':
+        print("폴더을 추가 하세요")
+        messagebox.showwarning("경고", "폴더을 추가 하세요")  # 파일 선택 안했을 때 메세지 출력
+        exit(1)
+    return "\\".join(folder.split("/"))
+
+
+
+file_path_list = []
+file_list = []
+dirname = select_folder("Frame_TimesStamp(이미지 관련)를 다 찾고자 하는 상위 폴더를 선택하세요")
+for (path, dir, files) in os.walk(dirname):
+    file_list.append([file for file in files if file.startswith("Frame_TimeStamp_")])
+    file_path_list.append([path + '\\' + file for file in files if file.startswith("Frame_TimeStamp_")])
+file_list = [v for v in file_list if v]
+file_path_list = [v for v in file_path_list if v]
+move_dirname = select_folder("Frame_TimesStamp를 복사할 곳을 선택하세요")
+file_new_list = [0 for i in range(len(file_list[0]))]
+for i in range(len(file_path_list)):
+    shutil.copy2(file_path_list[i][0], move_dirname + '\\' + file_list[i][0])
--- NIA_dataprocessing/procesing_and_matching_ver2.py
+++ NIA_dataprocessing/procesing_and_matching_ver2.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 from tkinter import filedialog
 from tkinter import messagebox
-import pickle
 import shutil
 
 def createFolder(directory):
@@ -45,27 +44,6 @@ def select_folder(str_):
         exit(1)
     return "\\".join(folder.split("/"))
 
-def search_all_Frame_TimeStamp_and_move2folder():
-    file_path_list = []
-    file_list = []
-    dirname = select_folder("Frame_TimesStamp(이미지 관련)를 다 찾고자 하는 상위 폴더를 선택하세요")
-    for (path, dir, files) in os.walk(dirname):
-        file_list.append([file for file in files if file.startswith("Frame_TimeStamp_")])
-        file_path_list.append([path + '\\' + file for file in files if file.startswith("Frame_TimeStamp_")])
-    file_list = [v for v in file_list if v]
-    file_path_list = [v for v in file_path_list if v]
-    move_dirname = select_folder("이미지(PNG) 파일이 있는 곳을 선택하세요")
-    print(file_path_list)
-    print(len(file_path_list))
-    for i in range(len(file_path_list)):
-        with open(file_path_list[i][0], 'r') as f:
-            lines = f.readlines()
-            for line_i in range(len(lines)):
-                lines[line_i] = lines[line_i].split("\t")[1].replace(' ', '').replace('\n', '') + '_' + str(line_i) + '\n'
-        with open(move_dirname +'\\new_' + file_list[i][0], 'w') as f:
-            f.writelines(lines)
-        file_list[i] = file_list[i][0]
-    return file_list, move_dirname
 
 def select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile():
     lidar_data_list_txtfile, lidar_data_list_txtfile_path = select_file(
@@ -79,7 +57,6 @@ def select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile():
         lidar_date_list = f.readlines()
     for i in range(len(lidar_date_list)):
         lidar_date_list[i] = '_'.join(lidar_date_list[i].split('\\')[-1].split('_')[1:3])[:-5]
-    print(lidar_date_list)
     for date_i in range(len(lidar_date_list)):
         pcd_file_list = [file for file in os.listdir(lidar_data_list_txtfile_path) if
                          (file.startswith(lidar_date_list[date_i]) and file.endswith('.pcd'))]
@@ -87,19 +64,42 @@ def select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile():
         with open(lidar_data_list_txtfile_path + "\\" + lidar_date_list[date_i] + "_lidar_TimeStamp.txt", 'w') as f:
             f.writelines(pcd_ts_list)
             print(lidar_data_list_txtfile_path + "\\" + lidar_date_list[
-                date_i] + "_lidar_TimeStamp.txt" + "{}th pcd ts list txt file maded!".format(date_i + 1))
-    for i in range(len(lidar_date_list)):
-        lidar_date_list[i] = lidar_date_list[i] + "_lidar_TimeStamp.txt"
+                date_i] + "_lidar_TimeStamp.txt" + "  {}th pcd ts list txt file maded!".format(date_i + 1))
+        lidar_date_list[date_i] = lidar_date_list[date_i] + "_lidar_TimeStamp.txt"
     return lidar_date_list, lidar_data_list_txtfile_path
 
 
-def make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path ,camera_ts_filelist, camera_ts_path):
+def search_all_Frame_TimeStamp_and_move2folder():
+    file_path_list = []
+    file_list = []
+    dirname = select_folder("Frame_TimesStamp(이미지 관련)가 모여있는 폴더를 선택하세요")
+    for (path, dir, files) in os.walk(dirname):
+        file_list.append([file for file in files if file.startswith("Frame_TimeStamp_")])
+        file_path_list.append([path + '\\' + file for file in files if file.startswith("Frame_TimeStamp_")])
+    file_list = [v for v in file_list if v]
+    file_path_list = [v for v in file_path_list if v]
+    move_dirname = select_folder("이미지(PNG) 파일이 있는 곳을 선택하세요")
+    file_new_list = [0 for i in range(len(file_list[0]))]
+    for i in range(len(file_path_list[0])):
+        with open(file_path_list[0][i], 'r') as f:
+            lines = f.readlines()
+            for line_i in range(len(lines)):
+                lines[line_i] = lines[line_i].split("\t")[1].replace(' ', '').replace('\n', '') + '_' + str(
+                    line_i) + '\n'
+        with open(move_dirname + '\\new_' + file_list[0][i], 'w') as f:
+            f.writelines(lines)
+        print("open {}  $$and make$$ {} !! \t {}th file".format(file_path_list[0][i], move_dirname + '\\new_' + file_list[0][i], i+1))
+        file_new_list[i] = file_list[0][i]
+    return file_list, move_dirname
+
+
+def make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path, camera_ts_path):
     match_list_path_list = []
     for file_i in range(len(lidar_ts_filelist)):
         with open(lidar_ts_path + '\\' + lidar_ts_filelist[file_i], 'r') as f:
             lidar_ts_list = f.readlines()
             lidar_ts_list = np.asarray(lidar_ts_list, dtype=np.uint32)
-            camera_ts_file = [file for file in os.listdir(camera_ts_path) if (file.endswith(('_').join(lidar_ts_filelist[file_i].split('_')[0:2]) + ".bin"))]
+            camera_ts_file = [file for file in os.listdir(camera_ts_path) if (file.endswith('_'.join(lidar_ts_filelist[file_i].split('_')[0:2]) + ".bin"))]
         with open(camera_ts_path + '\\' + camera_ts_file[0], 'r') as f:
             camera_ts_frame_list = f.readlines()
             camera_ts_list = [0 for i in range(len(camera_ts_frame_list))]
@@ -109,9 +109,7 @@ def make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path ,camera_ts_filelist
                 camera_fr_list[k] = camera_ts_frame_list[k].split('_')[1]
             camera_ts_list = np.asarray(camera_ts_list, dtype=np.uint32)
             camera_fr_list = np.asarray(camera_fr_list, dtype=np.uint32)
-
-
-        k,j,q,y = 0, 0 ,0 ,0
+        k, j, q, y = 0, 0, 0, 0
         range_ = 17
         lidar_matched_list = []
         camera_matched_tick_list = []
@@ -122,7 +120,6 @@ def make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path ,camera_ts_filelist
             mask_all = np.logical_and(mask_arr1, mask_arr2)
             tick = camera_ts_list[mask_all]
             frame = camera_fr_list[mask_all]
-
             if len(tick) == 0:
                 q = q + 1
             elif len(tick) == 1:
@@ -137,24 +134,18 @@ def make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path ,camera_ts_filelist
                 j = j + 1
             else:
                 y = y + 1
-        # print("q, k, j , y = ", q, k, j , y)
-        # print("matching : ", k + j + y)
-        # print(lidar_ts_filelist[file_i])
-        # print("len(camera_matched_tick_list), len(lidar_matched_list)", len(camera_matched_tick_list), len(lidar_matched_list))
-        with open(lidar_ts_path + '\\' + ('_').join(lidar_ts_filelist[file_i].split('_')[0:2]) + "_match_list.txt", 'w') as f:
+        with open(lidar_ts_path + '\\' + '_'.join(lidar_ts_filelist[file_i].split('_')[0:2]) + "_match_list.txt", 'w') as f:
+            print(lidar_ts_path + '\\' + '_'.join(lidar_ts_filelist[file_i].split('_')[0:2]) + "_match_list.txt\t{}th file maded!!".format(file_i+1))
             for i in range(len(lidar_matched_list)):
                 f.write(str(camera_matched_tick_list[i]) + '\t' + str(camera_matched_fr_list[i]) + '\t' +
                         str(lidar_matched_list[i]) + '\n')
-        match_list_path_list.append(lidar_ts_path + '\\' + ('_').join(lidar_ts_filelist[file_i].split('_')[0:2]) + "_match_list.txt")
+        match_list_path_list.append(lidar_ts_path + '\\' + '_'.join(lidar_ts_filelist[file_i].split('_')[0:2]) + "_match_list.txt")
     return match_list_path_list
 
 
-def extract_PCDPNGpair_by_matchlist(match_list_path_list, camera_ts_path, camera_ts_filelist):
-    png_move_dir = select_folder("매칭한 PNG 파일을 저장할 폴더를 선택하시오")
-    pcd_move_dir = select_folder("매칭한 PCD 파일을 저장할 폴더를 선택하시오")
-
+def extract_PCDPNGpair_by_matchlist(match_list_path_list, camera_ts_path, camera_ts_filelist, png_move_dir, pcd_move_dir):
+    match_list_i = 0
     for match_list in match_list_path_list:
-        print(match_list)
         camera_match_frame = []
         lidar_match_stamp = []
         with open(match_list, 'r') as f:
@@ -163,7 +154,6 @@ def extract_PCDPNGpair_by_matchlist(match_list_path_list, camera_ts_path, camera
             camera_match_frame.append(lines[i].split('\t')[1])
             lidar_match_stamp.append(lines[i].split('\t')[2].replace('\n', ''))
 
-        '_'.join(match_list.split('\\')[-1].split('_')[0:2])
         lidar_file_list = [file for file in os.listdir(camera_ts_path) if file.startswith('_'.join(match_list.split('\\')[-1].split('_')[0:2])) and file.endswith(".pcd")]
         camera_file_list = [file for file in os.listdir(camera_ts_filelist) if file.startswith('2_' + '_'.join(match_list.split('\\')[-1].split('_')[0:2])) and file.endswith(".png")]
         lidar_match_file_list = []
@@ -173,29 +163,25 @@ def extract_PCDPNGpair_by_matchlist(match_list_path_list, camera_ts_path, camera
             camera_match_file_list.append([file for file in camera_file_list if file.endswith(str(camera_match_frame[i]) + ".png")][0])
 
         for i in range(len(camera_match_file_list)):
+            print("coping {} / {} folder \t {} / {} files...".format(match_list_i+1, len(match_list_path_list), i+1, len(camera_match_file_list)))
             shutil.copy2(camera_ts_path + '\\' + lidar_match_file_list[i], pcd_move_dir + '\\' + '_'.join(lidar_match_file_list[i].replace('.pcd', '').split('_')[0:2])
                          + '_' + '{0:06d}'.format(i) + '.pcd')
             shutil.copy2(camera_ts_filelist + '\\' + camera_match_file_list[i], png_move_dir + '\\' + '1_' + '_'.join(camera_match_file_list[i].replace('.png', '').split('_')[1:3])
                          + '_' + '{0:06d}'.format(i) + '.png')
+        match_list_i = match_list_i + 1
 
 
 def main():
-    if 1:
-        lidar_ts_filelist, lidar_ts_path = select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile()
-        camera_ts_filelist, camera_ts_path = search_all_Frame_TimeStamp_and_move2folder()
-        with open('test2.pickle', 'wb') as f:
-            pickle.dump(lidar_ts_filelist, f)
-            pickle.dump(lidar_ts_path, f)
-            pickle.dump(camera_ts_filelist, f)
-            pickle.dump(camera_ts_path, f)
-    else:
-        with open('test2.pickle', 'rb') as f:
-            lidar_ts_filelist = pickle.load(f)
-            lidar_ts_path = pickle.load(f)
-            camera_ts_filelist = pickle.load(f)
-            camera_ts_path = pickle.load(f)
-    match_list_path_list = make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path, camera_ts_filelist, camera_ts_path)
-    extract_PCDPNGpair_by_matchlist(match_list_path_list, lidar_ts_path, camera_ts_path)
+    print("select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile.......")
+    lidar_ts_filelist, lidar_ts_path = select_lidar_data_list_txtfile_and_make_lidarTimeStamp_txtfile()
+    print("search_all_Frame_TimeStamp_and_move2folder.......")
+    camera_ts_filelist, camera_ts_path = search_all_Frame_TimeStamp_and_move2folder()
+    print("make_match_list_txtfile.......")
+    png_move_dir = select_folder("매칭한 PNG 파일을 저장할 폴더를 선택하시오")
+    pcd_move_dir = select_folder("매칭한 PCD 파일을 저장할 폴더를 선택하시오")
+    match_list_path_list = make_match_list_txtfile(lidar_ts_filelist, lidar_ts_path, camera_ts_path)
+    print("extract_PCDPNGpair_by_matchlist.......")
+    extract_PCDPNGpair_by_matchlist(match_list_path_list, lidar_ts_path, camera_ts_path, png_move_dir, pcd_move_dir)
 
 if __name__ == "__main__":
     main()
