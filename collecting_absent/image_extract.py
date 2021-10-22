a795ea797b068bf37375c15eb969677b142d8d90
lucas_korea <jcy37012@naver.com>
2021-10-22 16:41:54 +0900

    작업자 데이터 분배 코드

---
+++ collecting_absent/image_extract.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+# 영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
+path_dir = 'D:\\H2BUS_3차년도_DB_정제중(20211018)\\20210205_수소버스_도로주행영상(전후좌우)\\1번_전방'
+count = 0
+file_list = os.listdir(path_dir)
+print(file_list)
+for file in file_list:
+    print(file)
+    vidcap = cv2.VideoCapture(path_dir + '\\' + file)
+    count = 0
+    while (vidcap.isOpened()):
+        ret, image = vidcap.read()
+        # 캡쳐된 이미지를 저장하는 함수
+        if (count % 30 == 0):
+            try:
+                cv2.imwrite("D:\\20210205_1th_image\\" + file[:-4] + "%d.jpg" % count, image)
+                print('Saved frame%d.jpg' % count)
+            except:
+                break
+        count += 1
+    vidcap.release()
+print("finish")
--- collecting_absent/nia_data_distribute.py
+++ collecting_absent/nia_data_distribute.py
@@ -2,31 +2,23 @@ import os
 import random
 import shutil
 
-path = "G:\\정제완료5799장_전방"
+path = "D:\\공주대  gt 데이터 정제\\45_NIA3)Hep)_87_211007_pyeongtaek_2(44)\\Src_PNG"
+# 작업자명단 = ['성미애','성선영','신성례','노수진','오연주','윤가영', '노화중','박윤미','정성미','권미애','김민서','이민희']
+작업자명단 = ['강인선','고지연', '김다예', '배은이', '윤기주', '이상미', '정금연', '정다운', '정유림', '정혜림']
+for i in 작업자명단:
+    try:
+        os.mkdir(path + "\\" + i)
+    except:
+        pass
+list = [_ for _ in os.listdir(path) if _.endswith('.png')]
 
-os.mkdir("G:\\정제완료5799장_전방\\성미애")
-os.mkdir("G:\\정제완료5799장_전방\\성선영")
-os.mkdir("G:\\정제완료5799장_전방\\신성례")
-os.mkdir("G:\\정제완료5799장_전방\\노수진")
-os.mkdir("G:\\정제완료5799장_전방\\오연주")
-os.mkdir("G:\\정제완료5799장_전방\\노화중")
-os.mkdir("G:\\정제완료5799장_전방\\박윤미")
-os.mkdir("G:\\정제완료5799장_전방\\정성미")
-os.mkdir("G:\\정제완료5799장_전방\\이민희")
-os.mkdir("G:\\정제완료5799장_전방\\김민서")
-os.mkdir("G:\\정제완료5799장_전방\\심혜림")
-os.mkdir("G:\\정제완료5799장_전방\\서경옥")
-os.mkdir("G:\\정제완료5799장_전방\\노은영")
-os.mkdir("G:\\정제완료5799장_전방\\권미애")
+# random.shuffle(list)
+# for i in range(len(list)):
+#     print(i/len(list))
+#     shutil.copy2(path + '\\' + str(list[i]), path + '\\' + 작업자명단[i % len(작업자명단)])
 
-
-list = [_ for _ in os.listdir(path) if _.endswith('.jpg')]
-random.shuffle(list)
-print(list)
-print(len(list))
-
-작업자명단 = ['성미애','성선영','신성례','노수진','오연주','노화중','박윤미','정성미','이민희','김민서','심혜림','서경옥','노은영','권미애']
-for i in range(len(list)):
-    print(path + '\\' + str(list[i]))
-    print(path + '\\' + 작업자명단[i % len(작업자명단)])
-    shutil.copy2(path + '\\' + str(list[i]), path + '\\' + 작업자명단[i % len(작업자명단)])
+image_number = 185
+for i in range(len(작업자명단)):
+    for v in range(image_number):
+        print(v + i * image_number, '/', len(작업자명단) * image_number)
+        shutil.move(path + '\\' + str(list[v + i * image_number]), path + '\\' + 작업자명단[i])
