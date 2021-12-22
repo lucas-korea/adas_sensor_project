eeafa20bf91f5d3e1bb652a14eb683572c86c4d2
lucas_korea <jcy37012@naver.com>
2021-12-22 12:58:59 +0900

    F

--- utils/nia_data_distribute.py
+++ utils/nia_data_distribute.py
@@ -4,8 +4,8 @@ import shutil
 path = "D:\\GT 생성 업무\\객체생성-검수\\공주대  gt 데이터 정제\\대구_전방_결과_CAM123\\3"
 # 작업자명단 = ['성미애','성선영','신성례','노수진','오연주','윤가영', '노화중','박윤미','정성미','권미애','김민서','이민희']
 # 작업자명단 = ['강인선','고지연', '김다예', '배은이', '윤기주', '이상미', '정금연', '정다운', '정유림', '정혜림']
-작업자명단 = ['김다예','정다운','윤가영','정성미','권미애' ]
-날짜 = '1217'
+작업자명단 = ['이민희' ]
+날짜 = '1221'
 image_number =400
 for i in 작업자명단:
     try:
@@ -15,6 +15,6 @@ for i in 작업자명단:
 list = [_ for _ in os.listdir(path) if _.endswith('.png') or _.endswith('.jpg')]
 for i in range(len(작업자명단)):
     for v in range(image_number):
-        print(작업자명단[i], 날짜, end='')
+        print(작업자명단[i], 날짜, end='__')
         print(v + i * image_number, '/', len(작업자명단) * image_number)
         shutil.move(path + '\\' + str(list[v + i * image_number]), path + '\\' + 작업자명단[i] + 날짜)
