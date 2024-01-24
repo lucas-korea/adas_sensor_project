a3a934264d8860253557165fb637c9905b65a5a1
lucas-korea <jcy37012@naver.com>
2024-01-24 17:44:04 +0900

    delete legacy files

--- Research_ouster/(nia)라이다경로만들기.bat
+++
@@ -1,2 +0,0 @@
-dir /s /b Lidar*.bin > %date:-=%%time:~0,2%%time:~3,2%_list.txt
-
--- polarize/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-path = 'E:\\최대인지거리 실험 lfit전달 데이터_2안'
-# for (root, dirs, files) in os.walk(path):
-
-file_list = os.listdir(path)
-for file in file_list:
-    os.rename(path + '\\' + file, path + '\\' + '_'.join(file.split('_')[:-1])  + '.png')
